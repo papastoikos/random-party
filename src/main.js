761d3485a0bfaa6e3e8ed0482b599d90ae574c60
papastoikos <papastoikosalexandros@outlook.com>
2020-04-11 04:45:59 +0300

    new element

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 $(document).ready(function()
 {
-    console.log("Ready.");
-    var s = document.createElement("script");
-    s.type = "text/javascript";
-    s.src = "src/game-element.js";
-    $("head").append(s);
-    let ge = new GameElement();
-    $("body").append(ge);
+    let gameElementClass = document.createElement("script");
+    gameElementClass.type = "text/javascript";
+    gameElementClass.src = "src/game-element.js";
+    $("head").append(gameElementClass);
 });
+let ge = new GameElement();
+$("body").append(ge);
