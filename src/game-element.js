1c2c88a274f30cd3ef50771ac72340bc731b87e2
papastoikos <papastoikosalexandros@outlook.com>
2020-04-11 05:06:54 +0300

    standard commit

--- src/game-element.js
+++ src/game-element.js
@@ -3,6 +3,7 @@ class GameElement extends HTMLElement
     constructor()
     {
         super();
+        $("body").css("background", "yellow");
     }
 }
 window.customElements.define("game-element", GameElement);
