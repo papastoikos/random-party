26c1cc6495ad1b78271b9b1797b06a5eee558a1d
papastoikos <papastoikosalexandros@outlook.com>
2020-04-11 17:54:13 +0300

    standard commit

--- src/game-constants.js
+++ src/game-constants.js
@@ -1,8 +1,8 @@
 // GAME VALUES
 const BOARD_ROWS = 7;
 const BOARD_COLUMNS = 7;
-const TILE_HEIGHT = 1;
-const TILE_WIDTH = 1;
+const TILE_HEIGHT = 7;
+const TILE_WIDTH = 7;
 
 // CSS CLASSES
 const BOARD_CLASS = "board";
