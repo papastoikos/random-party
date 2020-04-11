3f4e066c3423a0e2e7e327933900c51e3fd8ca3d
papastoikos <papastoikosalexandros@outlook.com>
2020-04-11 06:06:55 +0300

    standard commit

--- src/board-element.js
+++ src/board-element.js
@@ -3,6 +3,7 @@ class Board extends GameElement
     constructor(top = "auto", left = "auto")
     {
         super((BOARD_ROWS * TILE_HEIGHT), (BOARD_COLUMNS * TILE_WIDTH), top, left);
+        console.log(BOARD_ROWS);
     }
 }
 window.customElements.define("board-element", Board);
