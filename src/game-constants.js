b06da5ccad0d2345f46ce30102f7664f0bdefbb4
papastoikos <papastoikosalexandros@outlook.com>
2020-04-11 17:52:26 +0300

    standard commit

--- src/game-constants.js
+++ src/game-constants.js
@@ -1,8 +1,8 @@
 // GAME VALUES
 const BOARD_ROWS = 7;
 const BOARD_COLUMNS = 7;
-const TILE_HEIGHT = 50;
-const TILE_WIDTH = 50;
+const TILE_HEIGHT = 1;
+const TILE_WIDTH = 1;
 
 // CSS CLASSES
 const BOARD_CLASS = "board";
--- src/game-element.js
+++ src/game-element.js
@@ -25,7 +25,7 @@ class GameElement extends HTMLElement
         }
         else if(!(isNaN(val)))
         {
-            return val + "px";
+            return val + "vh";
         }
     }
 }
