e779b77a99f8b73ebeeead9a71d8aeb49b9e81f2
papastoikos <papastoikosalexandros@outlook.com>
2020-04-13 02:32:30 +0300

    trying to make control panel draggable

--- src/control-panel-element.js
+++ src/control-panel-element.js
@@ -36,7 +36,7 @@ class ControlPanel extends GameElement
         else
         {
             this.setContent(content);
-            $(this).animate({height: CONTROL_PANEL_HEIGHT}, 400);
+            $(this).animate({height: CONTROL_PANEL_HEIGHT}, 100);
             return true;
         }
     }
