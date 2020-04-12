adf7414fc8dd2f30e7419be3a5a098b515366dee
papastoikos <papastoikosalexandros@outlook.com>
2020-04-12 22:50:26 +0300

    trying to make control panel draggable

--- src/control-panel-element.js
+++ src/control-panel-element.js
@@ -4,6 +4,8 @@ class ControlPanel extends GameElement
     drag = false;
     initialTop;
     initialLeft;
+    pointerDX;
+    pointerDY;
     constructor(top = "auto", left = "auto", height = CONTROL_PANEL_HEIGHT, width = CONTROL_PANEL_WIDTH)
     {
         super(height, width, top, left);
@@ -21,18 +23,57 @@ class ControlPanel extends GameElement
     {
         this.addEventListener(
             "mousedown",
-            this.startDrag.bind(this)
+            this.startDrag.bind(this),
+            true
         );
     }
 
     startDrag(event)
     {
-        console.log(event);
+        event.preventDefault();
+        event.stopPropagation();
         this.drag = true;
         this.initialTop = parseFloat(this.offsetTop);
+        this.pointerDY = event.clientY - this.initialTop;
         this.initialLeft = parseFloat(this.offsetLeft);
-        console.log(`${this.initialTop}`);
-        console.log(`${this.initialLeft}`);
+        this.pointerDX = event.clientX = this.initialLeft;
+        this.addEventListener(
+            "mouseup",
+            true
+        );
+        this.addEventListener(
+            "mousemove",
+            this.dragMove.bind(this),
+            true
+        );
+    }
+
+    dragMove(event)
+    {
+        event.preventDefault();
+        event.stopPropagation();
+        if(this.drag)
+        {
+            console.log(`element-x: ${event.clientX - this.pointerDX}, element-y: ${event.clientY - this.pointerDY}`);
+            console.log(`cursor-x: ${event.clientX}, cursor-y: ${event.clientY}`);
+        }
+    }
+    
+    stopDrag(event)
+    {
+        event.preventDefault();
+        event.stopPropagation();
+        this.drag = false;
+        this.removeEventListener(
+            "mouseup",
+            this.stopDrag,
+            true
+        );
+        this.removeEventListener(
+            "mousemove",
+            this.dragMove,
+            true
+        );
     }
 }
 window.customElements.define("control-panel-element", ControlPanel);
