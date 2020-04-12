55e95d376f61610b9e984c74efd99966feabdd8b
papastoikos <papastoikosalexandros@outlook.com>
2020-04-13 01:18:46 +0300

    trying to make control panel draggable

--- src/control-panel-element.js
+++ src/control-panel-element.js
@@ -1,11 +1,11 @@
 class ControlPanel extends GameElement
 {
     displayType = "flex";
-    // drag = false;
-    // initialTop;
-    // initialLeft;
-    // pointerDX;
-    // pointerDY;
+    drag = false;
+    initialTop;
+    initialLeft;
+    pointerDX;
+    pointerDY;
     constructor(top = "auto", left = "auto", height = CONTROL_PANEL_HEIGHT, width = CONTROL_PANEL_WIDTH)
     {
         super(height, width, top, left);
@@ -16,7 +16,7 @@ class ControlPanel extends GameElement
         super.build();
         this.id = "control-panel";
         this.addCssClass(CONTROL_PANEL_CLASS);
-        // this.makeDraggable();
+        this.makeDraggable();
     }
 
     setContent(content)
@@ -51,76 +51,76 @@ class ControlPanel extends GameElement
         }
     }
 
-    // makeDraggable()
-    // {
-    //     this.addEventListener(
-    //         "mousedown",
-    //         this.startDrag.bind(this),
-    //         true
-    //     );
-    // }
+    makeDraggable()
+    {
+        this.addEventListener(
+            "mousedown",
+            this.startDrag.bind(this),
+            true
+        );
+    }
 
-    // startDrag(event)
-    // {
-    //     event.preventDefault();
-    //     event.stopPropagation();
-    //     if(event.target.id !== this.id)
-    //     {
-    //         return;
-    //     }
-    //     this.drag = true;
-    //     this.initialTop = parseFloat(this.offsetTop);
-    //     this.pointerDY = event.clientY - this.initialTop;
-    //     this.initialLeft = parseFloat(this.offsetLeft);
-    //     this.pointerDX = event.clientX - this.initialLeft;
-    //     this.addEventListener(
-    //         "mouseup",
-    //         this.stopDrag.bind(this),
-    //         true
-    //     );
-    //     this.addEventListener(
-    //         "mouseleave",
-    //         this.stopDrag.bind(this),
-    //         true
-    //     );
-    //     this.addEventListener(
-    //         "mousemove",
-    //         this.dragMove.bind(this),
-    //         true
-    //     );
-    // }
+    startDrag(event)
+    {
+        event.preventDefault();
+        event.stopPropagation();
+        if(event.target.id !== this.id)
+        {
+            return;
+        }
+        this.drag = true;
+        this.initialTop = parseFloat(this.offsetTop);
+        this.pointerDY = event.clientY - this.initialTop;
+        this.initialLeft = parseFloat(this.offsetLeft);
+        this.pointerDX = event.clientX - this.initialLeft;
+        this.addEventListener(
+            "mouseup",
+            this.stopDrag.bind(this),
+            true
+        );
+        this.addEventListener(
+            "mouseleave",
+            this.stopDrag.bind(this),
+            true
+        );
+        this.addEventListener(
+            "mousemove",
+            this.dragMove.bind(this),
+            true
+        );
+    }
 
-    // dragMove(event)
-    // {
-    //     event.preventDefault();
-    //     event.stopPropagation();
-    //     if(this.drag)
-    //     {
-    //         this.style.top = this.formatValue((event.clientY - this.pointerDY));
-    //         this.style.left = this.formatValue((event.clientX - this.pointerDX));
-    //     }
-    // }
+    dragMove(event)
+    {
+        event.preventDefault();
+        event.stopPropagation();
+        if(this.drag)
+        {
+            this.style.top = this.formatValue((event.clientY - this.pointerDY));
+            this.style.left = this.formatValue((event.clientX - this.pointerDX));
+        }
+    }
     
-    // stopDrag(event)
-    // {
-    //     event.preventDefault();
-    //     event.stopPropagation();
-    //     this.drag = false;
-    //     this.removeEventListener(
-    //         "mouseup",
-    //         this.stopDrag,
-    //         true
-    //     );
-    //     this.removeEventListener(
-    //         "mouseleave",
-    //         this.stopDrag,
-    //         true
-    //     );
-    //     this.removeEventListener(
-    //         "mousemove",
-    //         this.dragMove,
-    //         true
-    //     );
-    // }
+    stopDrag(event)
+    {
+        event.preventDefault();
+        event.stopPropagation();
+        this.drag = false;
+        this.removeEventListener(
+            "mouseup",
+            this.stopDrag,
+            true
+        );
+        this.removeEventListener(
+            "mouseleave",
+            this.stopDrag,
+            true
+        );
+        this.removeEventListener(
+            "mousemove",
+            this.dragMove,
+            true
+        );
+    }
 }
 window.customElements.define("control-panel-element", ControlPanel);
