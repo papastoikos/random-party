c3238b96d6419567271897f9549f767140b25de4
papastoikos <papastoikosalexandros@outlook.com>
2020-04-12 19:48:14 +0300

    working on control panel

--- src/screen-element.js
+++ src/screen-element.js
@@ -26,13 +26,7 @@ class Screen extends GameElement
 
     buildControlPanel()
     {
-        console.log(`window.offsetHeight -> ${window.offsetHeight}`);
-        console.log(`window.offsetWidth -> ${window.offsetWidth}`);
-        console.log(`document.offsetHeight -> ${document.offsetHeight}`);
-        console.log(`document.offsetWidth -> ${document.offsetWidth}`);
-        console.log(`document.documentElement.offsetHeight -> ${document.documentElement.offsetHeight}`);
-        console.log(`document.documentElement.offsetWidth -> ${document.documentElement.offsetWidth}`);
-        this.controlPanel = new ControlPanel(($("body").offsetHeight - CONTROL_PANEL_HEIGHT), ($("body").offsetWidth - CONTROL_PANEL_WIDTH));
+        this.controlPanel = new ControlPanel((document.body.clientHeight - CONTROL_PANEL_HEIGHT), (document.body.clientWidth - CONTROL_PANEL_WIDTH));
         this.controlPanel.build();
         this.appendChild(this.controlPanel);
     }
