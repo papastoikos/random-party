dd35a9db15d58a255f8457ed205f0475f63bf79d
papastoikos <papastoikosalexandros@outlook.com>
2020-04-12 22:09:48 +0300

    draggable control panel

--- src/control-panel-element.js
+++ src/control-panel-element.js
@@ -1,6 +1,9 @@
 class ControlPanel extends GameElement
 {
     displayType = "flex";
+    drag = false;
+    initialTop;
+    initialLeft;
     constructor(top = "auto", left = "auto", height = CONTROL_PANEL_HEIGHT, width = CONTROL_PANEL_WIDTH)
     {
         super(height, width, top, left);
@@ -11,6 +14,43 @@ class ControlPanel extends GameElement
         super.build();
         this.id = "control-panel";
         this.addCssClass(CONTROL_PANEL_CLASS);
+        this.makeDraggable();
+    }
+
+    makeDraggable()
+    {
+        this.addEventListener(
+            "mousedown",
+            this.startDrag.bind(this)
+        );
+    }
+
+    startDrag(event)
+    {
+        console.log(event);
+        this.drag = true;
+        this.initialTop = parseFloat(this.offsetTop);
+        this.initialLeft = parseFloat(this.offsetLeft);
+        console.log(`${this.initialTop}`);
+        console.log(`${this.initialLeft}`);
     }
 }
 window.customElements.define("control-panel-element", ControlPanel);
+
+function mouseDown(event)
+{
+    let drag = true;
+}
+
+function mouseMove(event)
+{
+    if(drag)
+    {
+        // move along with cursor.
+    }
+}
+
+function mouseUp()
+{
+    let drag = false;
+}
