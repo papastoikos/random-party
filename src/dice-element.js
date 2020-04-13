d23a0753543d9e97e1c8a1dd6d40acc21df13093
papastoikos <papastoikosalexandros@outlook.com>
2020-04-13 06:12:02 +0300

    trying to make control panel draggable

--- src/dice-element.js
+++ src/dice-element.js
@@ -1,17 +1,17 @@
 class Dice extends GameElement
 {
     displayType = "flex";
-    diceArray = [];
-    constructor(height = DICE_HEIGHT, width = DICE_WIDTH, top = "auto", left = "auto")
+    constructor(seqNumber, height = DICE_HEIGHT, width = DICE_WIDTH, top = "auto", left = "auto")
     {
         super(height, width, top, left);
+        this.seqNumber = seqNumber;
     }
 
     build()
     {
         super.build();
-        this.id = "dicetable";
-        this.addCssClass(DICETABLE_CLASS);
+        this.id = `dice-${this.seqNumber}`;
+        this.addCssClass(DICE_CLASS);
     }
 }
 window.customElements.define("dice-element", Dice);
--- src/dicetable-element.js
+++ src/dicetable-element.js
@@ -13,5 +13,16 @@ class Dicetable extends GameElement
         this.id = "dicetable";
         this.addCssClass(DICETABLE_CLASS);
     }
+
+    buildDice()
+    {
+        for(let i = 0; i < NUMBER_OF_DICE; i++)
+        {
+            let dice = new Dice(i);
+            dice.build();
+            this.diceArray.push(dice);
+            this.appendChild(dice);
+        }
+    }
 }
 window.customElements.define("dicetable-element", Dicetable);
